94332668d20eef64b23cc904675b90617a2713cb
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-01-07 23:13:50 -0300

    Adicionando recursos e informacoes

--- assets/js/carregar/carregar-cards-principal.js
+++ assets/js/carregar/carregar-cards-principal.js
@@ -47,5 +47,27 @@ export const carregarCardsPrincipal = () => {
     }
   });
   
+  const todosProjetos = [
+    ['php', 0],
+    ['mysql', 0],
+    ['js', 0],
+    ['css', 0],
+    ['html', 0]
+  ]
+
+  dadosProjetos.forEach(projeto => {
+    projeto.linguagens.forEach((linguagem) => {
+
+      todosProjetos.forEach((dados, index) => {
+        if(linguagem.toLowerCase() == todosProjetos[index][0]){  
+          todosProjetos[index][1] += 1;
+        }
+      })
+
+    })
+  })
+
+  console.log(todosProjetos)
+
   return listagemProjetos;
 }
