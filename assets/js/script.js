875fa64324713440713cc6ae01c857fbb20b9cb0
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-01-09 10:18:51 -0300

    Removendo o loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,13 +16,6 @@ btnFecharModais.forEach(elemento =>{
 })
 
 window.addEventListener("load", () => {
-
-  const spinner = document.querySelector(".spinner");
-        
-  setTimeout(() => {
-    spinner.style.display = 'none';
-  }, 1000);
-
   const areaImportacao = document.createElement('section');
   areaImportacao.setAttribute('data-area-importacao', '');
 
@@ -30,7 +23,6 @@ window.addEventListener("load", () => {
 
   const vLibras = "<div vw class='enabled'><div vw-access-button class='active'></div> <div vw-plugin-wrapper><div class='vw-plugin-top-wrapper'></div> </div></div><script src='https://vlibras.gov.br/app/vlibras-plugin.js'></script><script> new window.VLibras.Widget('https://vlibras.gov.br/app');</script>";
   
-  areaImportacao.innerHTML += carregamento;
   areaImportacao.innerHTML += modalDireitosAutorais;
   areaImportacao.innerHTML += vLibras;
   
