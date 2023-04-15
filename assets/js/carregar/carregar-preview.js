ce177b172d26177395dda14b0fd27d1ba0664c3a
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-04-14 21:16:35 -0300

    Corrigindo bug de exibição de feedback\

--- assets/js/carregar/carregar-preview.js
+++ assets/js/carregar/carregar-preview.js
@@ -22,16 +22,17 @@ export const carregarPreview = (btn) => {
   iframe.setAttribute('src', link);
 
   $(modalExibicaoPreview).modal('show');
+  const vazio = corpo.querySelector('[data-link-vazio]');
 
   //Verifica se o link está ou não vazio e exibe o feedback (se tiver vazio)
   if(isEmpty(link) || lengthZero(link)){
-    const vazio = corpo.querySelector('[data-link-vazio]');
     vazio.textContent = '';
     vazio.textContent = 'Não foi atribuído um link para este projeto'
     spinner.style.display = 'none';
   }
-
+  
   else{
+    vazio.textContent = '';
     iframe.addEventListener("load" , function(){
       iframe.classList.add('block');
       spinner.style.display = 'none';
