ccd70164647e003ce68dbe3415ae1489135454f4
Gabriel <105074547+gabrieszin@users.noreply.github.com>
2024-08-13 19:59:43 -0300

    feat: alterando conteúdo

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,108 @@
   const conteudos = {
     "skills": [
       {
-        "name": "",
+        "name": "HTML 5",
         "link" : "#",
-        "img": ""
+        "img": "assets/img/svgs/skills/html.svg",
+        "alt": "Logo do HTML 5"
+      },
+      {
+        "name": "Javascript",
+        "link" : "#",
+        "img": "assets/img/svgs/skills/js.svg",
+        "alt": "Logo do Javascript"
+      },
+      {
+        "name": "PHP",
+        "link" : "#",
+        "img": "assets/img/svgs/skills/php.svg",
+        "alt": "Logo do PHP"
+      },
+      {
+        "name": "Java",
+        "link" : "#",
+        "img": "assets/img/svgs/skills/java.svg",
+        "alt": "Logo do Java"
+      },
+      {
+        "name": "Node",
+        "link" : "#",
+        "img": "assets/img/svgs/skills/node.svg",
+        "alt": "Logo do Node"
+      },
+      {
+        "name": "React",
+        "link" : "#",
+        "img": "assets/img/svgs/skills/react.svg",
+        "alt": "Logo do React"
+      },
+      {
+        "name": "CSS",
+        "link" : "#",
+        "img": "assets/img/svgs/skills/css.svg",
+        "alt": "Logo do CSS"
+      },
+      {
+        "name": "Figma",
+        "link" : "#",
+        "img": "assets/img/svgs/skills/figma.svg",
+        "alt": "Logo do Figma"
+      },
+      {
+        "name": "CSS",
+        "link" : "#",
+        "img": "assets/img/svgs/skills/git.svg",
+        "alt": "Logo do Git"
       },
     ],
     "hobbies": [
       {
-        "name": "",
-        "img": ""
+        "name": "Ouvir música",
+        "img": "assets/img/svgs/hobbies/musica.svg",
+        "alt": "Ícone de música"
+      },
+      {
+        "name": "Assistir séries",
+        "img": "assets/img/svgs/hobbies/series.svg",
+        "alt": "Ícone ilustrativo de uma série"
+      },
+      {
+        "name": "Andar de bike",
+        "img": "assets/img/svgs/hobbies/bicicleta.svg",
+        "alt": "Ícone de uma bicileta"
+      },
+      {
+        "name": "Programar",
+        "img": "assets/img/svgs/hobbies/programar.svg",
+        "alt": "Sinais de menor que (<) e maior que (>)"
+      },
+      {
+        "name": "Assistir filmes",
+        "img": "assets/img/svgs/hobbies/cinema.svg",
+        "alt": "Ícone de uma tela"
       },
     ],
     "academic": [
       {
-        "name": "",
-        "school": "",
-        "start": "",
+        "name": "Técnico em Informática para Internet",
+        "school": "Escola Estadual Técnico Industrial Professor Fontes",
+        "start": "2021-07-01",
+        "finished": true,
+        "finish": "2022-12-31",
+        "alt": "Imagem ilustrativa de uma tela de computador",
+        "img": "#"
+      },
+      {
+        "name": "Análise e Desenvolvimento de Sistemas",
+        "school": "Uninter",
+        "start": "2023-07-01",
         "finished": false,
-        "finish": "",
-        "img": ""
+        "finish": "2026-01-01",
+        "alt": "",
+        "img": "#"
       },
     ],
+    // Import from gist
     "projects": [
       {
         "name": "",
