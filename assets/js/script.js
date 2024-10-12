50a8f0e08d28da073457e58f6f78481b239aeb95
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-10-12 17:12:28 -0300

    test: iniciando implementação da seção de serviços

--- assets/js/script.js
+++ assets/js/script.js
@@ -152,7 +152,7 @@ import { data as conteudos } from './data.js';
 
       getUniqueElements(ret.projects.filter(e => e.visible && e.active), 'name').forEach(project => {
         projectContainer.innerHTML += `
-          <div class="experiencie__box" data-aos="fade-up">
+          <div class="experience__box" data-aos="fade-up">
             <img class="experience__midia hover-scale" loading="lazy" src="${project.img || 'https://via.placeholder.com/600x400.png?text=Imagem...'}" alt="Captura de tela do projeto '${project.name.trim()}'">
             <div class="experience__info">
               <h2 class="experience__title">${project.name.trim()}</h2>
