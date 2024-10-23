544d4d5f00b911e8b85cd47d04338f741c273217
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-10-23 15:42:29 -0300

    feat: estizando footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,11 +7,14 @@ export default function Footer({ fnc }) {
       <div className="footer__rodape">
         <p className="destaque">Desenvolvido por Gabriel Ribeiro</p>
         {/* TODO - criar ação para acionar o dialog */}
-        <div><img src="./img/svgs/copyright.svg" alt="Copyright" />&nbsp;2022 - <span>{new Date().getFullYear()}</span>
-          <p><a className="link-visivel" data-action="show-modal">Direitos Autorais</a> | <a className="link-visivel"
-            onClick={fnc}>Ir para o topo</a></p>
+        <div style={{
+          display: 'flex', flexDirection: 'column', gap: '0.25rem', alignItems: 'center', justifyContent: 'center', textAlign: 'center',
+        }}>
+          <p><img src="./img/svgs/copyright.svg" alt="Copyright" />&nbsp;2022 - <span>{new Date().getFullYear()}</span></p>
+          <p><a className="link-visivel" data-action="show-modal">Direitos Autorais</a></p>
+          <p><a className="link-visivel" onClick={fnc}>Ir para o topo</a></p>
         </div>
-      </div>
+      </div >
     </footer >
   )
 }
