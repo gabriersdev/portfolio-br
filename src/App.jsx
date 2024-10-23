9a95f18b8937ef911e63e3afdfb70f36c1725e98
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-10-23 13:24:40 -0300

    feat: add. impressão no console

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Principal from './Principal'
 import Redirect from './Redirect'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    console.log(
+      "%cContrate-me!\nhttps://www.linkedin.com/in/gabrielribeirodev/",
+      "font-size: 2rem; color: #FFF; background-color: #2631FF; font-family: sans-serif;",
+    )
+  }, [])
+
   return (
     <>
       <BrowserRouter>
