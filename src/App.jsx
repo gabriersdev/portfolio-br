b1bb44b45f642d730d1f69044627945348034deb
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-10-23 13:00:17 -0300

    style: código

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import Principal from './Principal'
 import Redirect from './Redirect'
 
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/portfolio/" element={<Principal />} ></Route>
+          <Route path="/portfolio/" exact element={<Principal />} ></Route>
           <Route path="/portfolio/redirect/" element={<Redirect />} ></Route>
         </Routes>
       </BrowserRouter>
