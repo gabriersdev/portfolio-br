6961b869ca214356c5e9080fafbbe50766aa2d62
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-11-20 17:14:49 -0300

    feat: link para alternar visualização

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import './footer.css'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 export default function Footer({ fnc }) {
   return (
@@ -10,6 +11,7 @@ export default function Footer({ fnc }) {
           <p><img src="./img/svgs/copyright.svg" alt="Copyright" />&nbsp;2022 - <span>{new Date().getFullYear()}</span></p>
           <div className="footer__rodape__links">
             <a data-action="show-modal">Direitos Autorais</a>
+            <Link to={"/?or=freelancer"} data-element="ref-contact">Visualização p<span className='arial'>/</span> freelance</Link>
             <a onClick={fnc}>Ir para o topo</a>
           </div>
         </div>
