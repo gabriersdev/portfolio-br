0f3d35d3622524f05ee7297c56a1ab037229e905
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-01-12 19:58:33 -0300

    test: implementando service worker

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {useEffect} from "react"
+
 import Principal from './Principal'
 import Redirect from './Redirect'
 
 function App() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator && window.location.hostname !== "localhost") {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(() => console.log("Service Worker registrado com sucesso!"))
+        .catch((err) => console.error("Erro ao registrar o Service Worker:", err));
+    }
+  }, []);
+
   return (
     <>
       <BrowserRouter basename='/portfolio'>
         <Routes>
-          <Route path="/" element={<Principal />} ></Route>
-          <Route path="/redirect/" element={<Redirect />} ></Route>
+          <Route path="/" element={<Principal/>}></Route>
+          <Route path="/redirect/" element={<Redirect/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
